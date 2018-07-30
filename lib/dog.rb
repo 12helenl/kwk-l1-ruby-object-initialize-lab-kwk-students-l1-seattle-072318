d71c4068c82083de63ba53d428851838e7c7a0b8
12helenl <12helenl@gmail.com>
2018-07-30 16:28:27 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,18 @@
 # dog.rb
 
 class Dog 
+  def initialize(name,breed)
+    @name = name
+    @breed = breed
+  end
   
+  def name
+    @name = name
+  end
+  
+  def breed = (new_breed)
+    @breed = new_breed
+  end 
+  
+end
+
